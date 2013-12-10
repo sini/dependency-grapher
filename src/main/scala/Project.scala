4fc393da1d6d9180d296a50cf2632f87343624b5
Jason Bowman <sinistar@gmail.com>
2013-12-10 19:56:48 +0000

    fix typo

--- src/main/scala/Project.scala
+++ src/main/scala/Project.scala
@@ -41,7 +41,7 @@ case object System extends Scope
 
 case class Repository(id: String, url: String, name: String = "", layout: String = "default")
 
-case clasProject(
+case class Project(
   publishedArtifact: Artifact,
 //  packaging: Packaging,
 //  parent: Artifact,
